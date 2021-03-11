2d75f1320eba7c37e52fdf284ae9fc03598a462b
İçimi Demirağ <74818535+icimidemirag@users.noreply.github.com>
2021-03-11 16:39:04 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+#Create two lists. The first list should consist of odd numbers. The second list is also of even numbers.
+#Merge two lists. Multiply all values in the newlist by 2.
+#Use the loop to print the data type of the all values in the new list.
 
 #Question 1
-for x in range(a):
-	print(a)
+oddList = [1,3,5,7,9]
+evenList = [0,2,4,6,8]
+
+oddList.extend(evenList)
+
+newList = [x*2 for x in oddList]
+
+for i in newList: #newList elemanlarını yazdırır.
+    print(i, end=" ")
+
+print("\n")
+
+for i in newList: #newlist elemanlarının typelarını yazdırır.
+    print(type(i), end=" ")
